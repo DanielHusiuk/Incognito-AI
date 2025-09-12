6543a0ddce7d6769c5427acf89559e1d33b374a4
DanielHusiuk <danielhusiuk@gmail.com>
2025-09-12 23:45:52 +0300

    Initial Commit

---
+++ Incognito AI/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Incognito AI
+//
+//  Created by Daniel Husiuk on 12.09.2025.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
